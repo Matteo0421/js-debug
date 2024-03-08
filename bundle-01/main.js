6d2e5d4f128825b419d2f801989f1dfb1ca52589
Matteo0421 <matteo.carola99@gmail.com>
2024-03-08 15:27:21 +0100

    [end]bundle-1

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -14,34 +14,44 @@
 for (let i = 0; i > 5; i++) {
     console.log(i);
 }
-
+// è un esercizio che serve per incrementare l'indice di un contatore, non ha erorri ne di sintassi ne logici
 
 // ESERCIZIO 2
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) {
         return num + 5;
     }
     return num;
 }
+// errore nella condizione del if non puo andare solo un uguola perchè fa asseggnare, ne vanno 3
+// se il numero è dispari aggiungere 5
 
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
 }
 
+// ERRORI
+// sbagliata la sintassi per il ciclo for, vanno messi i ;
+// è un esercizio che serve per incrementare l'indice di un contatore
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
+    for (let i = 0; i < numbers.length - 1; i++) {
+        if (numbers % 2 === 0); {
             evenNumbers.push(i);
         }
         return evenNumbers;
     }
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+// esercizio 4
+
+// riga 45 bisogna togliere il ; dopo i++, a riga 46 sempre === e non =
+// L'esercizio ha il compito di pisciare i numeri pari all'interno dell'array vuoto
